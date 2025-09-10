6727809cf4e96aaee263c0b4937b40e06719bfae
Kapil <kapilraut026@gmail.com>
2025-09-10 21:54:47 +0545

    hello

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
